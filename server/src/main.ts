b5699d72152542ce06604d0e9297f55e2fce98ff
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2025-04-25 16:19:15 +0700

    Another file main.ts update

---
+++ server/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix('api');
+
+  app.set('trust proxy', true);
+
+  app.enableCors({
+    origin: JSON.parse(process.env.ALLOWED_DOMAINS as string) || [],
+    methods: 'GET,POST,DELETE, PUT',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  });
+
+
+  await app.listen(process.env.PORT || 4000);
+}
+bootstrap();
