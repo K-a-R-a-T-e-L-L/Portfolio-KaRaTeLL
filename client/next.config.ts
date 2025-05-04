46010dd0999bf6a6598f0edec950d23b2c9c93a4
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2025-05-04 16:36:05 +0700

    Adding image properties to the next configuration, which sets certain restrictions on the resolution of images in order to avoid errors with downloading them from the server

--- client/next.config.ts
+++ client/next.config.ts
@@ -14,7 +14,8 @@ const nextConfig: NextConfig = {
         hostname: process.env.NEXT_PUBLIC_HOSTNAME_SERVER as string,
         pathname: '/uploads/**'
       }
-    ]
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
 
