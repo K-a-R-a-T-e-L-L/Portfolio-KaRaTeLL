44b4a9902ac634f041c8c4a66417875ff58132fd
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2025-05-06 18:58:47 +0700

    Updating the next configuration to check if the error with uploading images on mobile devices disappears

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,18 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: process.env.NEXT_PUBLIC_HOSTNAME_SERVER as string,
-        port: '4000',
-        pathname: '/uploads/**'
-      },
-      {
         protocol: 'https',
         hostname: process.env.NEXT_PUBLIC_HOSTNAME_SERVER as string,
         pathname: '/uploads/**'
       }
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
   },
 };
 
