202761ef23f6b7ed942fc524cb88d66adc3187cc
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2025-05-06 19:52:48 +0700

    Again

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,9 +9,9 @@ const nextConfig: NextConfig = {
         pathname: '/uploads/**'
       }
     ],
-    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    // deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // minimumCacheTTL: 60,
   },
 };
 
