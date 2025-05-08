239f7d8f644f90b3e05bbb75d0d0e318c608f251
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2025-05-08 13:38:00 +0700

    Small changes for further testing whether the images will load correctly

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,17 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const logger = new Logger('ImageRequests');
+
+  app.use((req, res, next) => {
+    logger.log(`Request for ${req.url}`);
+    next();
+  });
 
   app.useStaticAssets(join(__dirname, '..', '..', 'uploads'), {
     prefix: '/uploads/',
@@ -16,6 +24,13 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.set('trust proxy', true);
+
+  app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    next();
+  });
 
   app.enableCors({
     origin: JSON.parse(process.env.ALLOWED_DOMAINS as string) || [],
